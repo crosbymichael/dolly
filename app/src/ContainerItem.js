aa65366a2202c64d5f475eed050aaca5926811c2
rrarunan <arunan@docker.com>
2015-08-13 13:33:25 -0700

    Add fill percentage to server display

--- app/src/ContainerItem.js
+++ app/src/ContainerItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SmoothieGraph from './SmoothieGraph';
+import classnames from 'classnames';
 
 export default class ContainerItem extends Component {
   render() {
@@ -7,10 +8,29 @@ export default class ContainerItem extends Component {
       color: 'white',
       backgroundColor: '#3c5164'
     };
+    var barStyle = {
+      transitionDuration: '300ms',
+      width: this.props.fillPct + '%'
+    };
+    var barClassnames = classnames({
+      'red': this.props.fillPct > 90,
+      'orange': this.props.fillPct <= 90 && this.props.fillPct > 70,
+      'yellow': this.props.fillPct <= 70 && this.props.fillPct > 50,
+      'olive': this.props.fillPct <= 50,
+      'ui': true,
+      'progress': true
+    });
     return (
       <div className="card">
         <div className="content">
-          <div className="header">{this.props.name || 'unknown'}</div>
+          <div className="header">
+            {this.props.name || 'unknown'}
+            <div className="right">
+              <div className={barClassnames} data-percent={this.props.fillPct}>
+                <div className="bar" style={barStyle}></div>
+              </div>
+            </div>
+          </div>
           <div className="description">
             <SmoothieGraph />
           </div>
@@ -27,5 +47,6 @@ export default class ContainerItem extends Component {
 }
 
 ContainerItem.propTypes = {
-  name: React.PropTypes.string
+  name: React.PropTypes.string,
+  fillPct: React.PropTypes.number
 }
